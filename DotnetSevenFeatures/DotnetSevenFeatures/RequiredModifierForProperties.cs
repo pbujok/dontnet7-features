b3835a6118fa2386e867d25d0316a976a28945d3
Piotr Bujok <piotr.bujok@sharp-soft.pl>
2022-12-10 13:50:48 +0100

    Use Json in tests

--- DotnetSevenFeatures/DotnetSevenFeatures/RequiredModifierForProperties.cs
+++ DotnetSevenFeatures/DotnetSevenFeatures/RequiredModifierForProperties.cs
@@ -5,5 +5,27 @@ public class RequiredModifierForProperties
     [Fact]
     public void Test1()
     {
+        // Try to remove properties initialization
+        var person = new Person()
+        {
+            FirstName = "Paul",
+            LastName = "Atreides"
+        };
+    }
+
+    public class Person
+    {
+        public required string FirstName { get; init; }
+        public required string LastName { get; init; }
+
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
--- DotnetSevenFeatures/DotnetSevenFeatures/StringMultiline.cs
+++ DotnetSevenFeatures/DotnetSevenFeatures/StringMultiline.cs
@@ -1,9 +1,41 @@
-﻿namespace DotnetSevenFeatures;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace DotnetSevenFeatures;
 
 public class StringMultiline
 {
     [Fact]
-    public void Test1()
+    public void UsingJsonInCSharpCode()
     {
+        // Arrange
+        var data = """
+            [
+                {
+                    "name": "Luke Skywalker",
+                    "isRealPerson": false
+                },
+                {
+                    "name": "Nikola Tesla",
+                    "isRealPerson": true
+                },
+                {
+                    "name": "Gandalf The Gray",
+                    "isRealPerson": true
+                },
+                {
+                    "name": "Walter E. Kurtz",
+                    "isRealPerson": false
+                }
+            ]
+            """;
+        
+        // Act
+        var deserialized = JsonConvert.DeserializeObject<ICollection<Person>>(data);
+
+        // Assert
+        deserialized!.Count.Should().Be(4);
     }
+    
+    private record Person(string Name, bool IsRealPerson);
 }
