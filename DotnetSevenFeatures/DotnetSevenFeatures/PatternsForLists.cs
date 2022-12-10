d714b6e590deef10f08ce30444adce0d82910e24
Piotr Bujok <piotr.bujok@sharp-soft.pl>
2022-12-10 13:57:39 +0100

    present pattern matching

--- DotnetSevenFeatures/DotnetSevenFeatures/PatternsForLists.cs
+++ DotnetSevenFeatures/DotnetSevenFeatures/PatternsForLists.cs
@@ -1,9 +1,19 @@
-﻿namespace DotnetSevenFeatures;
+﻿using FluentAssertions;
+
+namespace DotnetSevenFeatures;
 
 public class PatternsForLists
 {
     [Fact]
-    public void Test1()
+    public void PatternMatchingWorksForLists()
     {
+        // Arrange
+        int[] numbers = { 1, 2, 3 };
+
+        // Act & Assert
+        (numbers is [1, 2, 3]).Should().BeTrue();
+        (numbers is [1, 2, 4]).Should().BeFalse();
+        (numbers is [1, 2, 3, 4]).Should().BeFalse();
+        (numbers is [0 or 1, <= 2, >= 3]).Should().BeTrue();
     }
 }
